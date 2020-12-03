11ba1bc5a5d1f238575dd5c96db1dab3cec47cc5
vholmer <viktorholmer@gmail.com>
2020-12-03 22:13:02 +0100

    changed to slope based algorithm

--- 3/main.py
+++ 3/main.py
@@ -3,58 +3,31 @@ def get_input():
 
 	return f.readlines()
 
-def part1(data):
+def solve(data, slope_x, slope_y):
 	x_pos = 0
-
 	num_trees = 0
 
-	for line in data:
+	for y_pos, line in enumerate(data):
+		if y_pos != 0 and y_pos % slope_y != 0:
+			continue
+
 		width = len(line) - 1
 
 		if line[x_pos] == "#":
 			num_trees += 1
-			marker = line[:x_pos] + "X" + line[x_pos + 1:]
-		else:
-			marker = line[:x_pos] + "O" + line[x_pos + 1:]
 
-		x_pos += 3
+		x_pos += slope_x
 		x_pos %= width
 
 	return num_trees
-
-def part2(data):
-	# hspeed, vspeed. part 1 is 3, 1
-	slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
-
-	result = 1
-
-	for hspeed, vspeed in slopes:
-		x_pos = 0
-		num_trees = 0
-
-		for y_pos, line in enumerate(data):
-			if y_pos != 0 and y_pos % vspeed != 0:
-				continue
-
-			width = len(line) - 1
-
-			if line[x_pos] == "#":
-				num_trees += 1
-
-			x_pos += hspeed
-			x_pos %= width
-
-		result *= num_trees
-
-	return result
 
 def main():
 	data = get_input()
 
 	print("Part 1:")
-	print("Answer: ", part1(data))
+	print("Answer: ", solve(data, 3, 1))
 
 	print("Part 2:")
-	print("Answer: ", part2(data))
+	print("Answer: ", solve(data, 1, 1) * solve(data, 3, 1) * solve(data, 5, 1) * solve(data, 7, 1) * solve(data, 1, 2))
 
 main()
